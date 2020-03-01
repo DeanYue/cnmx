660069fe000c2c36141c31bda4315a5c02c30ffa
Dean Yue <yueqin@odooyun.com>
2020-03-01 16:48:12 +0800

    [Add]l10n_cn: add country data& city data.

--- addons/l10n_cn/__manifest__.py
+++ addons/l10n_cn/__manifest__.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-# Copyright (C) 2008-2008 凯源吕鑫 lvxin@gmail.com   <basic chart data>
-#                         维智众源 oldrev@gmail.com  <states data>
-# Copyright (C) 2012-2012 南京盈通 ccdos@intoerp.com <small business chart>
-# Copyright (C) 2008-now  开阖软件 jeff@osbzr.com    < PM and LTS >
-# Copyright (C) 2018-now  jeffery9@gmail.com
+# Part of Cnmx. See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'China - Accounting',
     'version': '1.8',
     'category': 'Localization',
-    'author': 'www.openerp-china.org',
-    'maintainer': 'jeff@osbzr.com',
-    'website': 'http://openerp-china.org',
+    'author': 'Cnmx Ltd.',
+    'maintainer': 'dean@cnmx.com',
+    'website': 'https://www.cnmx.com',
     'description': """
 Includes the following data for the Chinese localization
 ========================================================
@@ -23,9 +17,10 @@ Account Type/科目类型
 State Data/省份数据
 
     """,
-    'depends': ['base', 'account', 'l10n_multilang', 'base_city_address'],
+    'depends': ['base', 'account', 'l10n_multilang', 'base_address_city'],
     'data': [
         'data/account_tax_group_data.xml',
+        'data/res_country_data.xml',
         'data/res_city_data.xml',
     ],
 }
